a9134d4d7246a291b10f6982022786518f93904c
Shahzaib0217 <shahzaib.khan0217@gmail.com>
2023-02-06 22:58:03 +0500

    second commit

---
+++ backend/contants.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const CRYPTODEVS_NFT_CONTRACT_ADDRESS =
+    "0x9C4A8dC0fe61ddAdAb908AAf05b0f8b10aC94922";
+
+module.exports = { CRYPTODEVS_NFT_CONTRACT_ADDRESS };
